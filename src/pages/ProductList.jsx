8751cef2bc0429487fa1100045a5652062479607
jessicamillenia <jessicamillenia@gmail.com>
2022-02-23 12:26:16 +0700

    fetch product list

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Grid, Button } from "@mui/material";
 
 export default function ProductList() {
+    const [products, setProducts] = useState([]);
+  
+    useEffect(() => {
+        async function fetchProducts() {
+            fetch('http://localhost/penjualan/getproduct.php')
+            .then((res) => res.json())
+            .then((res) => setProducts(res.message));  
+        }
+        fetchProducts();
+    }, []);
     return (
         <div style={{textAlign:"center"}}>
             <h1>Product List</h1>
+            {products.map((product) => (
             <Grid container
                 direction="row"
                 justifyContent="center"
                 alignItems="center"
                 spacing={1}
-                padding={1}>
-                <Grid item lg={3} md={3} sm={3} xs={3}>
+                padding={1}
+                >
+                <Grid item key={product.id} lg={3} md={3} sm={3} xs={3}>
                     <img src={"https://via.placeholder.com/150"} width="100%" alt="" />
                 </Grid>
-                <Grid item lg={6} md={6} sm={6} xs={6} sx={{textAlign:"left"}}>   
-                    <p>SO Klin Pewangi</p>
-                    <p>Rp coret</p>
-                    <p>Rp asli</p>
+                <Grid item key={product.id} lg={6} md={6} sm={6} xs={6} sx={{textAlign:"left"}}>   
+                    <p>{product.name}</p>
+                    <p style={{textDecoration: "line-through"}}>{product.discount}</p>
+                    <p>{product.price}</p>
                 </Grid>
-                <Grid item lg={3} md={3} sm={3} xs={3}>   
+                <Grid item key={product.id} lg={3} md={3} sm={3} xs={3}>   
                     <Button color='warning' sx={{ width: '100%'}} variant='contained'>
                         Buy
                     </Button>
-                </Grid>
-                <Grid item lg={3} md={3} sm={3} xs={3}>
-                    <img src={"https://via.placeholder.com/150"} width="100%" alt="" />
-                </Grid>
-                <Grid item lg={6} md={6} sm={6} xs={6} sx={{textAlign:"left"}}>   
-                    <p>SO Klin Pewangi</p>
-                    <p>Rp coret</p>
-                    <p>Rp asli</p>
-                </Grid>
-                <Grid item lg={3} md={3} sm={3} xs={3}>   
-                    <Button color='warning' sx={{ width: '100%'}} variant='contained'>
-                        Buy
-                    </Button>
-                </Grid>
+                </Grid> 
             </Grid>
+            ))}
             <Button color='warning' sx={{ width: '90%'}} variant='contained'>
                 Checkout
             </Button>
