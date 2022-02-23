5c4d4c31f70c16cfe76cfb19cf5ecf1e0c6d2e86
jessicamillenia <jessicamillenia@gmail.com>
2022-02-23 11:30:49 +0700

    checkout & report UI

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Grid, Button } from "@mui/material";
 
 export default function CheckoutPage() {
     return (
-        <h1>Checkout</h1>
+        <div style={{textAlign:"center"}}>
+            <h1>Checkout Page</h1>
+            <Grid container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={1}
+                padding={1}>
+                <Grid item lg={3} md={3} sm={3} xs={3}>
+                    <img src={"https://via.placeholder.com/150"} width="100%" alt="" />
+                </Grid>
+                <Grid item lg={9} md={9} sm={9} xs={9} sx={{textAlign:"left"}}>   
+                    <p>SO Klin Pewangi</p>
+                    <p>1 PCS</p>
+                    <p>Subtotal: 123456789</p>
+                </Grid>
+                <Grid item lg={3} md={3} sm={3} xs={3}>
+                    <img src={"https://via.placeholder.com/150"} width="100%" alt="" />
+                </Grid>
+                <Grid item lg={9} md={9} sm={9} xs={9} sx={{textAlign:"left"}}>   
+                    <p>SO Klin Pewangi</p>
+                    <p>1 PCS</p>
+                    <p>Subtotal: 123456789</p>
+                </Grid>
+                <Grid item lg={3} md={3} sm={3} xs={3}>
+                    <img src={"https://via.placeholder.com/150"} width="100%" alt="" />
+                </Grid>
+                <Grid item lg={9} md={9} sm={9} xs={9} sx={{textAlign:"left"}}>   
+                    <p>SO Klin Pewangi</p>
+                    <p>1 PCS</p>
+                    <p>Subtotal: 123456789</p>
+                </Grid>
+            </Grid>
+            <h4>Total: 2340987</h4>
+            <Button color='warning' sx={{ width: '90%'}} variant='contained'>
+                Confirm
+            </Button>
+        </div>
     );
 }
--- src/pages/ReportPenjualan.jsx
+++ src/pages/ReportPenjualan.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
-export default function ReportPenjualan() {
-    return (
-        <h1>Report Penjualan</h1>
-    );
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Trx A', 159, 6.0, 24, 4.0),
+  createData('Trx B', 237, 9.0, 37, 4.3),
+  createData('Trx C', 262, 16.0, 24, 6.0),
+  createData('Trx D', 305, 3.7, 67, 4.3),
+  createData('Trx E', 356, 16.0, 49, 3.9),
+];
+
+export default function DenseTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 300 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Transaction</TableCell>
+            <TableCell align="right">User</TableCell>
+            <TableCell align="right">Total</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Item</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
