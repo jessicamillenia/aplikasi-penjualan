5afb20f5a222c17f7b3aa3b2c5ecb18904fee37e
jessicamillenia <jessicamillenia@gmail.com>
2022-02-23 10:43:54 +0700

    initial project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, Component, Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Login from "../pages/Login";
+import ProductList from "../pages/ProductList";
+import ProductDetail from "../pages/ProductDetail";
+import ReportPenjualan from "../pages/ReportPenjualan";
+import CheckoutPage from "../pages/CheckoutPage";
+
+class Home extends Component {
+  render() {
+    return (
+      <Router>
+          <Fragment>
+            <MyAppBar/>
+            <Routes>
+              <Route path="/" exact element={<ProductList/>} />
+              <Route path="/login" exact element={<Login/>} />
+              <Route path="/product/:id" exact element={<ProductDetail/>} />
+              <Route path="/report" exact element={<ReportPenjualan/>} />
+              <Route path="/checkout" exact element={<CheckoutPage/>} />
+            </Routes>
+          </Fragment>
+      </Router>
+    );
+  }
+}
+
+export default Home;
